0173eccc00f300749e62cc0fdb3dd848d53653b6
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <94269419+MRLUCKYXD@users.noreply.github.com>
2022-07-08 21:57:12 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**ᴜᴘʟᴏᴀᴅᴇʀ🥀 :-  [ᴅᴇᴠᴜ-ʀᴏʙᴏᴛ🧚‍♀️](https://t.me/DEVU_ROBOT)**"
+        rep = f"**ᴜᴘʟᴏᴀᴅᴇʀ🥀 :-  [ʟᴜᴄᴋʏ-ʀᴏʙᴏᴛ🧚‍♀️](https://t.me/lucky_officialbot)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
